6eeabcd17db3d72292ad5992d590c9b97aeb7fbd
mikanfactory <k952i4j14x17@gmail.com>
2026-03-02 00:57:14 +0900

    fix bugs

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,8 +4,8 @@ import { runMigrations } from "./migrate";
 export async function initializeDatabase(): Promise<void> {
   await runMigrations();
   const prisma = getPrismaClient();
-  await prisma.$executeRawUnsafe("PRAGMA journal_mode=WAL");
-  await prisma.$executeRawUnsafe("PRAGMA busy_timeout=5000");
+  await prisma.$queryRawUnsafe("PRAGMA journal_mode=WAL");
+  await prisma.$queryRawUnsafe("PRAGMA busy_timeout=5000");
 }
 
 export async function closeDatabase(): Promise<void> {
